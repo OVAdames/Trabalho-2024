d3dfece0691ec65b6e60411a1d11a62e24d79cb5
OV.Adames33 <129545051+OVAdames@users.noreply.github.com>
2024-04-27 15:57:36 -0300

    Add files via upload

---
+++ click.js
@@ -0,0 +1,12 @@
+var curtidasContador = 0;
+
+    function aumentarCurtidas() {
+        curtidasContador++;
+        document.getElementById("curtidas").innerText = curtidasContador;
+    }
+
+ var deslikeContador = 0;
+    function aumentardeslike() {
+        deslikeContador++;
+        document.getElementById("deslike").innerText = deslikeContador;
+    }
